f64e0cc2f881335478ed83b51552636285591c17
Martin Chovanec <chovanec.m11@gmail.com>
2025-03-31 02:17:51 +0200

    data imported, correct basic rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./example-data.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log(data[0]);
+    console.log(data[0].children);
+    console.log(data[0].children.has_nemesis?.records, "teest");
+    return (
+        <div className="App">
+            <div>
+                {data.map((item, index) => (
+                    <div key={index} style={{ marginBottom: "20px" }}>
+                        {Object.entries(item.data).map(([key, value]) => (
+                            <div key={key}>
+                                <strong>{key}:</strong> {value}
+                            </div>
+                        ))}
+                        {item.children.has_nemesis?.records?.map((item, index) => (
+                            <div key={index} style={{ marginBottom: "20px" }}>
+                                {Object.entries(item.data).map(([key, value]) => (
+                                    <div key={key}>
+                                        <strong>{key}:</strong> {value}
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                        {item.children.has_nemesis?.records[0]?.children?.has_secrete?.records?.map((item, index) => (
+                            <div key={index} style={{ marginBottom: "20px" }}>
+                                {Object.entries(item.data).map(([key, value]) => (
+                                    <div key={key}>
+                                        <strong>{key}:</strong> {value}
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+                <p></p>
+            </div>
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                Learn React
+            </a>
+        </div>
+    );
 }
 
 export default App;
